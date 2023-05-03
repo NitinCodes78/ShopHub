7e993a2f69e8acbc32916cefc33f354348b82aac
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-05-03 17:16:09 +0530

    cart quantity

--- populate-cart.js
+++ populate-cart.js
@@ -5,12 +5,29 @@ function populateList(products, productList) {   //products will be an array of 
         return `
         <li>
             <div class="img"><img src=${product.bgImgUrl}></div>
-            <div class="price">${product.price}</div>
+            <div class="price">Rs ${product.price}</div>
             <div class="quantity"> <input type="number" min="1" value="1"> </div>
-            <div class="total-amt">${product.price}</div>
+            <div class="total-amt">Rs ${product.price}</div>
         </li> 
         `;
     }).join('');
 }
-
 populateList(cartItems, itemList);
+
+
+/*````````````````````QUANTITY X PRICE``````````````````````````` */
+
+const quanInputs = [...document.querySelectorAll('.quantity input')] ;
+console.log(quanInputs);
+
+quanInputs.forEach(input =>{
+    input.addEventListener('change',(e)=>{
+        const target = e.target.parentElement.parentElement;
+
+        const quantity = parseInt(input.value) ;
+        let price = parseInt(target.querySelector('.price').innerText.slice(3));
+        let totalAmount = target.querySelector('.total-amt');
+        
+        totalAmount.innerText =  `Rs ${price * quantity}`;
+    })
+})
