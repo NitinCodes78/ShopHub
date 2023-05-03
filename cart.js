d54e984c38e9fd4753adc6e69f742f19c5a72cb6
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-05-03 15:09:33 +0530

    populate cart

--- cart.js
+++ cart.js
@@ -1,30 +1,22 @@
+const itemList = document.querySelector('.items-list');
 let cartItems = JSON.parse(localStorage.getItem('CART-ITEMS')) || [];
 
 const cartIcons = [...document.querySelectorAll('.cart')];
-
 cartIcons.forEach(icon => {
     icon.addEventListener('click',(e)=>{
         e.preventDefault;
         let targetProduct = e.target.parentElement.parentElement;
-
         const productId = targetProduct.id;
-        const price = targetProduct.querySelector('.price').innerText;
+        const price = parseInt(targetProduct.dataset.price);
         const styles = window.getComputedStyle(targetProduct, false);
         const bgImgUrl = styles.backgroundImage.slice(4, -1).replace(/"/g, "");
-
         const newItem = {
             productId,
             bgImgUrl,
             price
         };
-
-
         cartItems.push(newItem);
-        console.log(cartItems);
         localStorage.setItem('CART-ITEMS', JSON.stringify(cartItems)); 
-        
+
     })
-    
 });
-
-
