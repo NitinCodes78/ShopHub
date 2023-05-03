71007e4a314972b33e5b548157d462b5c4d2f63d
NitinCodes78 <nitingoyal8650@gmail.com>
2023-05-03 11:50:06 +0530

    a commit

--- products.js
+++ products.js
@@ -1,3 +1,4 @@
+/**************Showing the price and cart, heart, search image upon hovering the product************ */
 const products=document.querySelectorAll('.product');
 products.forEach(product=>product.addEventListener("mouseover",()=>{
      product.style.opacity='0.75';
@@ -15,3 +16,32 @@ products.forEach(product=>product.addEventListener("mouseout",()=>{
      const price=product.querySelector('.price');
      price.style.display='none';
 }));
+
+
+/**********Sorting the products in ascending and Descending Order*************** */
+function comparatorAscending(a,b){
+     if (parseInt(a.dataset.price) < parseInt(b.dataset.price))
+        return -1;
+     if (parseInt(a.dataset.price) > parseInt(b.dataset.price))
+        return 1;
+    return 0;
+}
+function comparatorDescending(a,b){
+     if (parseInt(a.dataset.price) < parseInt(b.dataset.price))
+        return 1;
+     if (parseInt(a.dataset.price) > parseInt(b.dataset.price))
+        return -1;
+    return 0;
+}
+function sortAscending(){
+     var prices=document.querySelectorAll('.product[data-price]');
+     var pricesArray=Array.from(prices);
+     let sorted=pricesArray.sort(comparatorAscending);
+     sorted.forEach(x=>document.querySelector('.products').appendChild(x)); 
+}
+function sortDescending(){
+     var prices=document.querySelectorAll('.product[data-price]');
+     var pricesArray=Array.from(prices);
+     let sorted=pricesArray.sort(comparatorDescending);
+     sorted.forEach(x=>document.querySelector('.products').appendChild(x));     
+}
