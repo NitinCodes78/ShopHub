e55c21e07e4f6738ce2ff5104d85eac1f4aa946f
NitinCodes78 <nitingoyal8650@gmail.com>
2023-05-03 21:32:35 +0530

    added js functionality for sorting according to price

--- products.js
+++ products.js
@@ -33,6 +33,13 @@ function comparatorDescending(a,b){
         return -1;
     return 0;
 }
+function comparatorNewest(a,b){
+     if (parseInt(a.id.slice(3)) < parseInt(b.id.slice(3)))
+        return -1;
+     if (parseInt(a.id.slice(3)) > parseInt(b.id.slice(3)))
+        return 1;
+    return 0;
+}
 function sortAscending(){
      var prices=document.querySelectorAll('.product[data-price]');
      var pricesArray=Array.from(prices);
@@ -44,4 +51,23 @@ function sortDescending(){
      var pricesArray=Array.from(prices);
      let sorted=pricesArray.sort(comparatorDescending);
      sorted.forEach(x=>document.querySelector('.products').appendChild(x));     
+}
+function sortNewest(){ //Sort according to the id(assuming it for the newest)
+     var prices=document.querySelectorAll('.product[data-price]');
+     var pricesArray=Array.from(prices);
+     let sorted=pricesArray.sort(comparatorNewest);
+     pricesArray.forEach(x=>document.querySelector('.products').appendChild(x)); 
+
+}
+/********************Event Listener for sorting according to price********* */
+const filter2=document.querySelector('.filter2');
+filter2.addEventListener("change", funcFilter2);
+function funcFilter2(){
+     const value=document.querySelector('.filter2 select').value;
+     if(value==='asc')
+       sortAscending();
+     else if(value==='dec')
+       sortDescending();
+     else 
+       sortNewest();
 }
