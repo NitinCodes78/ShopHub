21ae7ae85cf8ec75a39a63fe1eb2730634c78b24
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-05-03 15:19:01 +0530

    populate cart

--- cart.js
+++ cart.js
@@ -1,41 +1,26 @@
-const itemList = document.querySelector('.items-list');
 let cartItems = JSON.parse(localStorage.getItem('CART-ITEMS')) || [];
-
 const cartIcons = [...document.querySelectorAll('.cart')];
+
+
 cartIcons.forEach(icon => {
     icon.addEventListener('click',(e)=>{
         e.preventDefault;
         let targetProduct = e.target.parentElement.parentElement;
+
         const productId = targetProduct.id;
         const price = parseInt(targetProduct.dataset.price);
         const styles = window.getComputedStyle(targetProduct, false);
         const bgImgUrl = styles.backgroundImage.slice(4, -1).replace(/"/g, "");
+
         const newItem = {
             productId,
             bgImgUrl,
             price
         };
         cartItems.push(newItem);
-        console.log(cartItems);
-
-        populateList(cartItems, itemList);
         localStorage.setItem('CART-ITEMS', JSON.stringify(cartItems));   
         
     })
 });
 
 
-function populateList(products, productList) {   //products will be an array of objects
-    productList.innerHTML = products.map((product, i) => {
-        return `
-        <li>
-            <div class="img"><img src=${product.bgImgUrl}></div>
-            <div class="price">${product.price}</div>
-            <div class="quantity"><input type="number" min="1" value="1"></div>
-            <div class="total-amt">${product.price}</div>
-        </li> 
-        `;
-    }).join('');
-}
-
-populateList(cartItems, itemList)
