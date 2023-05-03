8fb9318d4853c939ec982f6ea645296e864681e3
NitinCodes78 <nitingoyal8650@gmail.com>
2023-05-03 23:22:06 +0530

    the commit

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ products.forEach(product=>product.addEventListener("mouseover",()=>{
      product.style.backgroundColor='grey';
      const images=product.querySelectorAll('.icon img');
      images.forEach(image=> image.style.display='flex');
+     const price=product.querySelector('.price');
+     price.style.display='inline-block';
 }));
 products.forEach(product=>product.addEventListener("mouseout",()=>{
     product.style.opacity='1';
      product.style.backgroundColor='rgb(220, 241, 241)';
      const images=product.querySelectorAll('.icon img');
      images.forEach(image=> image.style.display='none');
+     const price=product.querySelector('.price');
+     price.style.display='none';
 }));
 
 /*``````````````SLIDER````````````````````````*/
