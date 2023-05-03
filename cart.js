47a37390a0b7b6e44a6ec27b6f2499883169b308
NitinCodes78 <nitingoyal8650@gmail.com>
2023-05-03 12:49:01 +0530

    commit

--- cart.js
+++ cart.js
@@ -1,46 +1,32 @@
-/* const itemList = document.querySelector('.items-list');
-let cartItems = JSON.parse(localStorage.getItem('CART-ITEMS')) || [];
-
+const itemList = document.querySelector('ul.items-list');
+let cartItems = [];
 const cartIcons = [...document.querySelectorAll('.cart')];
-
 cartIcons.forEach(icon => {
     icon.addEventListener('click',(e)=>{
         let targetProduct = e.target.parentElement.parentElement;
-
         const productId = targetProduct.id;
-        const price = targetProduct.querySelector('.price').innerText;
+        const price = parseInt(targetProduct.dataset.price);
         const styles = window.getComputedStyle(targetProduct, false);
         const bgImgUrl = styles.backgroundImage.slice(4, -1).replace(/"/g, "");
-
         const newItem = {
             productId,
             bgImgUrl,
             price
         };
-
-
         cartItems.push(newItem);
-        console.log(cartItems);
-
+        console.log(itemList);
         populateList(cartItems, itemList);
-        localStorage.setItem('CART-ITEMS', JSON.stringify(cartItems));   
-        
+        // localStorage.setItem('CART-ITEMS', JSON.stringify(cartItems));      
     })
-    
 });
-
-
 function populateList(products, productList) {   //products will be an array of objects
     productList.innerHTML = products.map((product, i) => {
-        return `
-        <li>
-        <img src= ${product.bgImgUrl} >
-        <div class="cart-price">${product.price}</div>
-        </li>   
-        `;
+        return 
+        `<li>
+          <div class="img"><img src=${product.bgImgUrl}></div>
+          <div class="price">${product.price}</div>
+          <div class="quantity"><input type="number" min="1" value="1"></div>
+          <div class="total-amt">${product.price}</div>
+        </li>`
     }).join('');
 }
-
-populateList(cartItems, itemList)
-
- */
