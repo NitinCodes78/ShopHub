e8951f68e2ddb6c27a943be7a0908925bc32fe28
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-05-12 23:59:41 +0530

    login

--- node-mysql/controllers/auth.js
+++ node-mysql/controllers/auth.js
@@ -1,4 +1,39 @@
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('becryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+
+});
+
+
 exports.register = (req, res) =>{
     console.log(req.body);
+
+    const{ name, email, password, passwordConfirm} = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error,results) =>{
+        if(error){
+            console.log(error);
+        }
+        if(results.length >0){
+            return res.render('register',{
+                message: 'That Email is already in use'
+            })
+        }else if(password !== passwordConfirm){
+            return res.render('register',{
+                message:'Passwords do not match'
+            })
+        }
+        let hashedPassword = await bcrypt.hash(password, 5);
+
+
+
+    });
+
     res.send("Form submitted");
 }
